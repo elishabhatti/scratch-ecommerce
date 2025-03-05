7fee7d5ad9681c5dc9ac0e2e5120b8509719f994
elishabhatti <elishakumari2007@gmail.com>
2025-03-05 13:52:21 +0500

    modified users post method

--- app.js
+++ app.js
@@ -11,30 +11,48 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+const authenticateUser = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) return res.redirect("/");
+
+    const decoded = jwt.verify(token, "secret");
+    req.user = await userModel.findById(decoded.id).select("-password");
+    next();
+  } catch (error) {
+    res.status(401).send("Unauthorized");
+  }
+};
+
 app.get("/", (req, res) => {
   res.render("index");
+});
+app.get("/shop", authenticateUser, (req, res) => {
+  res.render("shop", {user: req.user});
 });
 
 app.post("/register", async (req, res) => {
   try {
     let { fullname, email, password, address, contact } = req.body;
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(password, salt, async (err, hash) => {
-        if (err) return res.status(401).send(err.message);
-        else {
-          let user = await userModel.create({
-            fullname,
-            email,
-            password: hash,
-            address,
-            contact,
-          });
-          const token = jwt.sign({ email: user.email, id: user._id }, "secret");
-          res.cookie("token", token);
-          res.render("shop")
-        }
-      });
+
+    let existingUser = await userModel.findOne({ email });
+    if (existingUser) return res.send("Try with differnt Email");
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    let user = await userModel.create({
+      fullname,
+      email,
+      password: hashPassword,
+      address,
+      contact,
     });
+
+    const token = jwt.sign({ email: user.email, id: user._id }, "secret");
+    res.cookie("token", token, { httpOnly: true });
+
+    res.redirect("/shop");
   } catch (error) {
     res.status(500).send(error.message);
   }
