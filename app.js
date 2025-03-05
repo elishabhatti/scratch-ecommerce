56a663a3d92794189c50ece118579c99c01ea711
elishabhatti <elishakumari2007@gmail.com>
2025-03-05 13:27:10 +0500

    complete the post method for users

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import { userModel } from "./models/user.models.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const app = express();
 app.use(express.json());
@@ -14,9 +16,28 @@ app.get("/", (req, res) => {
 });
 
 app.post("/register", async (req, res) => {
-    let {fullname, email, password, address, contact} = req.body;
-    let createdUser = await userModel.create({fullname, email, password, address, contact})
-    res.send(createdUser)
+  try {
+    let { fullname, email, password, address, contact } = req.body;
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(password, salt, async (err, hash) => {
+        if (err) return res.status(401).send(err.message);
+        else {
+          let user = await userModel.create({
+            fullname,
+            email,
+            password: hash,
+            address,
+            contact,
+          });
+          const token = jwt.sign({ email: user.email, id: user._id }, "secret");
+          res.cookie("token", token);
+          res.render("shop")
+        }
+      });
+    });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 app.listen(3000);
