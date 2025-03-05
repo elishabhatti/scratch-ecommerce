355d9c28adafbd9c0987e946212fad9ce12afa85
elishabhatti <elishakumari2007@gmail.com>
2025-03-05 12:14:44 +0500

    added middlewares

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(express.json());
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("HI");
