d65b2b049a9d58046794dc07248e01191accdd4d
elishabhatti <elishakumari2007@gmail.com>
2025-03-08 14:54:44 +0500

    added totalAmoun in the update product dynamically

--- controllers/updateProduct.controller.js
+++ controllers/updateProduct.controller.js
@@ -3,10 +3,11 @@ import { orderModel } from "../models/order.models.js";
 export const updateProduct = async (req, res) => {
   try {
     const orderId = req.params.id;
-    const { quantity, paymentMethod } = req.body;
+    const { quantity, paymentMethod, totalAmount } = req.body;
     await orderModel.findByIdAndUpdate(orderId, {
       quantity,
       paymentMethod,
+      totalAmount
     });
     res.redirect("/buyed-products");
   } catch (error) {
