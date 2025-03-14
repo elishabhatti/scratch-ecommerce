7c249cf805b7d757ee001e4dd292caa22dc25e14
elishabhatti <elishakumari2007@gmail.com>
2025-03-14 11:36:20 +0500

    desgined the cart page

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -37,21 +37,17 @@ router.post("/update-product/:id", authenticateUser, updateProduct);
 
 router.post("/add-to-cart", authenticateUser, async (req, res) => {
   const { productId } = req.body;
-
   try {
     const user = await userModel.findById(req.user._id);
     if (!user) return res.status(404).json({ message: "User not found" });
-
     const cartItem = user.cart.find((item) => item.bagId.toString() === productId);
-
     if (cartItem) {
-      cartItem.quantity += 1;
+      cartItem.quantity = 1;
     } else {
       user.cart.push({ bagId: productId, quantity: 1 });
     }
-
     await user.save();
-    res.status(200).json({ message: "Product added to cart", cart: user.cart });
+    res.redirect("/cart")
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error", error });
