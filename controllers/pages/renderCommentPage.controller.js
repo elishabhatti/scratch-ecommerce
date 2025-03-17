a09589201e932c7d89b7acef0c1a153015547e60
elishabhatti <elishakumari2007@gmail.com>
2025-03-17 16:36:03 +0500

    showed all comments

--- controllers/pages/renderCommentPage.controller.js
+++ controllers/pages/renderCommentPage.controller.js
@@ -1,9 +1,11 @@
 import { orderModel } from "../../models/order.models.js";
 
-export const rednerCommentPage =  async (req, res) => {
+export const renderCommentPage =  async (req, res) => {
   try {
     const order = await orderModel.findById(req.params.orderId);        
-    res.render("commentOnProduct", { id: order._id, userId: order.userId, order });
+    const orders = await orderModel.find({}, "comments").populate("comments.userId");
+    const allComments = orders.flatMap(order => order.comments);
+    res.render("commentOnProduct", { id: order._id, userId: order.userId, order, allComments });
   } catch (error) {
     res.status(500).send("Server Error");
   }
--- routes/userRouter.routes.js
+++ routes/userRouter.routes.js
@@ -2,14 +2,14 @@ import { Router } from "express";
 import { authenticateUser } from "../middlewares/authMiddleware.middleware.js";
 import { renderProfilePage } from "../controllers/pages/renderProfileUpdatePage.controller.js";
 import { updateUserProfile } from "../controllers/users/updateUserProfile.controller.js";
-import { rednerCommentPage } from "../controllers/pages/renderCommentPage.controller.js";
+import { renderCommentPage } from "../controllers/pages/renderCommentPage.controller.js";
 import { commentOnPage } from "../controllers/orders/commentOnBag.controller.js";
 
 const userRouter = Router();
 
 userRouter.get("/update-profile", authenticateUser, renderProfilePage);
 userRouter.post("/update-profile", authenticateUser, updateUserProfile);
-userRouter.get("/comment-on-product/:orderId", rednerCommentPage);
-userRouter.post("/submit-comment", commentOnPage);
+userRouter.get("/comment-on-product/:orderId", authenticateUser, renderCommentPage);
+userRouter.post("/submit-comment", authenticateUser, commentOnPage);
 
 export default userRouter;
